ff044a915533e569be0cace4ce8beb22e6499cb1
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-12-20 10:21:53 -0300

    Terminado

---
+++ js/punto4.js
@@ -0,0 +1,6 @@
+const num1 = prompt('Ingrese un número por favor');
+const num2 = prompt('Ingrese un número por favor');
+
+const resultado = parseFloat(num1) + parseFloat(num2);
+
+alert(`La suma es: ${resultado.toFixed(2)}`);
